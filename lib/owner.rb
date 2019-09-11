0a6f79479ea4504578b35bcda28ac4b559ae84a1
Laura Berge <lauraeberge@gmail.com>
2019-09-11 20:50:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,7 +54,7 @@ class Owner
   end
   
   def list_pets
-    p "I have #{@owner.dogs.count}dog(s), and #{@owner.cats.count} cat(s)."
+    p "I have #{dogs.length}dog(s), and #{cats.length} cat(s)."
   end
   
   def self.all
